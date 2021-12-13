d1f6e180dc39a5692a5c9b93f7009b0300afc2fe
shivam danawale <shivamdanawale@gmail.com>
2021-12-13 17:33:25 +0530

    Added tailwind css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <div>This is a home page</div>;
+  return (
+    <div>
+      <p className="bg-gray-300">This is a home page</p>
+    </div>
+  );
 }
 
 export default Home;
