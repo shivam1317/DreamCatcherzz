34eb0a4af1c5ba442bb108dd38c9f811e33000ec
unknown <furqanamiri@gmail.com>
2021-12-13 22:27:35 +0530

    changes in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function About() {
-  return <div>This is a about page</div>;
+  return <div>
+  <img src="" alt="The team of Dreamcatcherzzz" width="800" height="220"></img>
+  <h1><p>About
+    them
+    , to 
+    be 
+    researched 
+    and asked
+  </p>
+  </h1>
+  <p>
+
+
+  </p>
+  <p> icons 
+  <a href="https://www.linkedin.com">
+    <img src="" alt="Linkedin site" > </img> 
+    </a>
+    &emsp; &emsp; &emsp;
+    <a href="https://www.instagram.com">
+    <img src="" alt="Intsa site" > </img> 
+    </a>
+    &emsp; &emsp; &emsp;
+    <a href="https://www.youtube.com">
+    <img src="" alt="Youtube" > </img> 
+    </a>
+    </p>
+  </div>;
 }
 
 export default About;
