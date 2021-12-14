8f1cda6bbd7b86acd867aef6bae813a6d656ffc6
shivam danawale <shivamdanawale@gmail.com>
2021-12-14 16:24:34 +0530

    Changes in about by shivam

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 
 function About() {
-  return <div>
-  <img src="" alt="The team of Dreamcatcherzzz" width="800" height="220"></img>
-  <h1><p>About
-    them
-    , to 
-    be 
-    researched 
-    and asked
-  </p>
-  </h1>
-  <p>
-    Achievemnts and magazine reviews
-
-  </p>
-  <p> icons 
-  <a href="https://www.linkedin.com">
-    <img src="" alt="Linkedin site" > </img> 
-    </a>
-    &emsp; &emsp; &emsp;
-    </p>
-  </div>;
+  return (
+    <div>
+      <img
+        src=""
+        alt="The team of Dreamcatcherzzz"
+        width="800"
+        height="220"
+      ></img>
+      <p>About them , to be researched and asked</p>
+      <p>Achievemnts and magazine reviews</p>
+      <p>
+        {" "}
+        icons
+        <a href="https://www.linkedin.com">
+          <img src="" alt="Linkedin site" />
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default About;
