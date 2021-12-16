2f99d47345042595e833ef634a773b12de21db68
shivam danawale <shivamdanawale@gmail.com>
2021-12-16 16:10:16 +0530

    Slideshow on home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "../css/home.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper/core";
 
+import "swiper/components/navigation/navigation.min.css"; // Navigation module
+import "swiper/components/pagination/pagination.min.css"; // Pagination module
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 function Home() {
   return (
-    <div className="h-screen w-screen" id="container">
-      <p className="my-3">This is a home page</p>
+    <div className="h-screen" id="container">
+      <Swiper
+        slidesPerView={1}
+        centeredSlides
+        navigation={true}
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide className="mySwiperSlide">
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="random" />
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="random" />
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="random" />
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="random" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
@@ -11,5 +10,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-reportWebVitals();
