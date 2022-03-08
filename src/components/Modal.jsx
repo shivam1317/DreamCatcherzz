db2323b634193ba673fd04cc52566f785e168fdc
shivam danawale <shivamdanawale@gmail.com>
2022-03-08 13:40:26 +0530

    Changed modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,10 +23,10 @@ const Modal = ({ open, onClose }) => {
         ? ReactDOM.createPortal(
             <div className="overlay">
               <animated.div style={animation}>
-                <div className="modal">
+                <form className="modal">
                   {/* <button onClick={onClose}>Close</button> */}
-                  <div className="flex w-full justify-between p-4 border border-black text-2xl">
-                    <span>This is my text</span>
+                  <div className="flex w-full justify-between p-4 outline-none text-2xl bg-red-500 text-white">
+                    <span>Contact the designers</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-6 w-6 cursor-pointer"
@@ -43,62 +43,36 @@ const Modal = ({ open, onClose }) => {
                       />
                     </svg>
                   </div>
-                  <div>
-                    <form
-                      className="w-full max-w-lg mt-5"
+                  <div className="flex justify-evenly p-5">
+                    <div className="flex flex-col justify-evenly items-center w-2/5">
+                      <img
+                        src="./images/interior.jpg"
+                        alt="interior"
+                        className="h-2/3 w-full rounded-lg"
+                      />
+                      <div class="flex justify-around w-full">
+                        <input
+                          className="bg-red-500 h-5 w-5 text-white"
+                          type="checkbox"
+                          value=""
+                          id="flexCheckDefault"
+                          name="checkboxUpdate"
+                        />
+                        <label
+                          class="inline-block text-gray-800 text-md"
+                          for="flexCheckDefault"
+                        >
+                          Send updates on whatsapp
+                        </label>
+                      </div>
+                    </div>
+                    <div
+                      className="w-1/2 max-w-lg"
                       // ref={form}
                       // onSubmit={closePopup}
                       autoComplete="off"
                     >
-                      <div className="w-full px-3 mb-6 md:mb-0 ">
-                        <label
-                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-first-name"
-                        >
-                          Name
-                        </label>
-                        <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                          id="grid-first-name"
-                          type="text"
-                          placeholder="Jane Doe"
-                          required
-                          name="name"
-                        />
-                      </div>
-                      <div className="w-full px-3 mb-6 md:mb-0 ">
-                        <label
-                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-first-name"
-                        >
-                          Email
-                        </label>
-                        <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                          id="grid-first-name"
-                          type="email"
-                          placeholder="user@gmail.com"
-                          required
-                          name="email"
-                        />
-                      </div>
-                      <div className="w-full px-3 mb-6 md:mb-0 ">
-                        <label
-                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-first-name"
-                        >
-                          Phone no
-                        </label>
-                        <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                          id="grid-first-name"
-                          type="number"
-                          placeholder="Phone no..."
-                          required
-                          name="phone"
-                        />
-                      </div>
-                      <div class="w-full px-3 mb-6 md:mb-0">
+                      <div class="w-full px-3 my-3 md:mb-0">
                         <label
                           class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                           for="grid-state"
@@ -128,19 +102,97 @@ const Modal = ({ open, onClose }) => {
                           </div>
                         </div>
                       </div>
+                      <div class="w-full px-3 my-3 md:mb-0">
+                        <label
+                          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          for="grid-state"
+                        >
+                          Location
+                        </label>
+                        <div class="relative">
+                          <select
+                            class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-state"
+                            name="spaceType"
+                          >
+                            <option>Mumbai</option>
+                            <option>Pune</option>
+                            <option>Ahemadabad</option>
+                            <option>Bangalore</option>
+                            <option>Hydrabad</option>
+                          </select>
+                          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg
+                              class="fill-current h-4 w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                            >
+                              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full px-3 my-3 md:mb-0 ">
+                        <label
+                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          for="grid-first-name"
+                        >
+                          Name
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                          id="grid-first-name"
+                          type="text"
+                          placeholder="Jane Doe"
+                          required
+                          name="name"
+                        />
+                      </div>
+                      {/* <div className="w-full px-3 mb-6 md:mb-0 ">
+                        <label
+                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          for="grid-first-name"
+                        >
+                          Email
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                          id="grid-first-name"
+                          type="email"
+                          placeholder="user@gmail.com"
+                          required
+                          name="email"
+                        />
+                      </div> */}
+                      <div className="w-full px-3 my-3 md:mb-0 ">
+                        <label
+                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          for="grid-first-name"
+                        >
+                          Phone no
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                          id="grid-first-name"
+                          type="number"
+                          placeholder="Phone no..."
+                          required
+                          name="phone"
+                        />
+                      </div>
 
                       <div class="lg:w-full md:w-1/3 px-3 mb-6 md:mb-0 mt-3">
                         <button
-                          className="uppercase bg-red-600 text-white font-bold hover:text-red-600 hover:bg-white border-2 hover:border-red-600 w-full py-1 px-3 transition-all delay-75 rounded-xl text-center"
+                          className="uppercase bg-red-500 text-white font-bold hover:text-red-600 hover:bg-white border-2 hover:border-red-600 w-full py-1 px-3 transition-all delay-75 rounded-xl text-center"
                           type="submit"
                           onClick={closePopup}
                         >
                           Submit
                         </button>
                       </div>
-                    </form>
+                    </div>
                   </div>
-                </div>
+                </form>
               </animated.div>
             </div>,
             document.getElementById("portal")
