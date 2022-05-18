48a05146dd45c7739e5314e255a58cde32704ac2
shivam danawale <shivamdanawale@gmail.com>
2022-05-18 10:19:08 +0530

    Changes in modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -141,28 +141,14 @@ const Modal = ({ open, onClose }) => {
                         >
                           Location
                         </label>
-                        <div class="relative">
-                          <select
-                            class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-state"
-                            name="location"
-                          >
-                            <option>Mumbai</option>
-                            <option>Pune</option>
-                            <option>Ahemadabad</option>
-                            <option>Bangalore</option>
-                            <option>Hydrabad</option>
-                          </select>
-                          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg
-                              class="fill-current h-4 w-4"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                            >
-                              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                          </div>
-                        </div>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                          id="grid-first-name"
+                          type="text"
+                          placeholder="City Name"
+                          required
+                          name="name"
+                        />
                       </div>
                       <div className="w-full px-3 my-3 md:mb-0 ">
                         <label
